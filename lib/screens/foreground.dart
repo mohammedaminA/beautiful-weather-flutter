dfe7a4e7b3069b1494d5e10cca77f3072fba8541
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-21 02:12:50 +0300

    UI

--- lib/screens/foreground.dart
+++ lib/screens/foreground.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ForeGround extends StatelessWidget {
   @override
@@ -16,7 +17,8 @@ class ForeGround extends StatelessWidget {
         actions: [
           IconButton(
             icon: CircleAvatar(
-              backgroundImage: NetworkImage('https://i.ibb.co/Z1fYsws/profile-image.jpg'),
+              backgroundImage:
+                  NetworkImage('https://i.ibb.co/Z1fYsws/profile-image.jpg'),
               backgroundColor: Colors.black54,
             ),
             onPressed: () {},
@@ -24,8 +26,32 @@ class ForeGround extends StatelessWidget {
         ],
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 20.0),
-      ) ,
+        padding: EdgeInsets.symmetric(horizontal: 18.0),
+        child: DefaultTextStyle(
+          style: GoogleFonts.raleway(color: Colors.white),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Hello Mat!',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Check the weather for every city!',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(height: 35,),
+              TextField()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
