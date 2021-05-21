4ab3df599c6da01128610706ee10992949ea3b8d
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-21 03:08:14 +0300

    UI

---
+++ lib/models/location.dart
@@ -0,0 +1,9 @@
+class Location {
+  final String text;
+  final String time;
+  final int temperature;
+  final String weather;
+  final String imgUrl;
+
+  Location({this.text, this.time, this.temperature, this.weather, this.imgUrl});
+}
