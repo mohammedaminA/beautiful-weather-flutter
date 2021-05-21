c333d7ce2731275c145629c6c133f779d5dfb5d5
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-22 00:54:47 +0300

    Google Authentication

---
+++ lib/models/networking.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  NetworkHelper({this.url});
+   String url;
+
+  Future getData() async {
+    var uri = Uri.parse(url);
+    http.Response response = await http.get(uri);
+    if(response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    }
+    else {
+      print('error');
+    }
+  }
+}
