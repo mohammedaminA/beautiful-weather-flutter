04033ebc2427486207926273aed56ed9dba1382c
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-22 00:36:22 +0300

    Google Authentication

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,10 +1,1 @@
-class Location {
-  final String text;
-  final String time;
-  final int temperature;
-  final String weather;
-  final String imgUrl;
-
-  Location({this.text, this.time, this.temperature, this.weather, this.imgUrl});
-}
-
+import '';
