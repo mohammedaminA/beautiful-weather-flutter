5b162265f2290ac7e934b34b7fdc0b7e75d9a34d
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-21 17:33:00 +0300

    Google Authentication

--- lib/screens/foreground.dart
+++ lib/screens/foreground.dart
@@ -52,7 +52,7 @@ class ForeGround extends StatelessWidget {
               SizedBox(
                 height: 5,
               ),
-              Text(
+              Text(a
                 'Check the weather for every city!',
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
               ),
