d204c08f83161936d10b5f5a1d97e50d4fd16e92
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-23 00:13:12 +0300

    Google Authentication

--- lib/screens/foreground.dart
+++ lib/screens/foreground.dart
@@ -106,54 +106,54 @@ class ForeGround extends StatelessWidget {
                 SizedBox(
                   height: 30,
                 ),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //   children: [
-                //     for (var location in locations)
-                //       ClipRRect(
-                //         borderRadius: BorderRadius.circular(8),
-                //         child: Stack(
-                //           alignment: AlignmentDirectional.center,
-                //           children: [
-                //             ColorFiltered(
-                //               colorFilter: ColorFilter.mode(
-                //                   Colors.black45, BlendMode.darken),
-                //               child: Image.network(
-                //                 location.imgUrl,
-                //                 height: height * 0.35,
-                //               ),
-                //             ),
-                //             Column(
-                //               children: [
-                //                 Text(
-                //                   location.text,
-                //                   style: TextStyle(
-                //                       color: Colors.white,
-                //                       fontWeight: FontWeight.w600,
-                //                       fontSize: 19),
-                //                 ),
-                //                 Text(location.time),
-                //                 SizedBox(
-                //                   height: 40,
-                //                 ),
-                //                 Text(
-                //                   location.temperature.toString() + '°',
-                //                   style: TextStyle(
-                //                       color: Colors.white,
-                //                       fontSize: 40,
-                //                       fontWeight: FontWeight.w600),
-                //                 ),
-                //                 SizedBox(
-                //                   height: 50,
-                //                 ),
-                //                 Text(location.weather)
-                //               ],
-                //             )
-                //           ],
-                //         ),
-                //       )
-                //   ],
-                // )
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    for (var location in locations)
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: [
+                            ColorFiltered(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.black45, BlendMode.darken),
+                              child: Image.network(
+                                location.imgUrl,
+                                height: height * 0.35,
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  location.text,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 19),
+                                ),
+                                Text(location.time),
+                                SizedBox(
+                                  height: 40,
+                                ),
+                                Text(
+                                  location.temperature.toString() + '°',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  height: 50,
+                                ),
+                                Text(location.weather)
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                  ],
+                )
               ],
             ),
           ),
